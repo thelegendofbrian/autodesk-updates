09bf6f23d8071013802d397da66984b52d2d86ae
Brian <thelegendofbrian@users.noreply.github.com>
2023-06-18 17:30:37 -0700

    handles no updates found

--- script.js
+++ script.js
@@ -12,51 +12,58 @@ const url = "https://script.google.com/macros/s/AKfycbyAsnlDFFIuLL8ZK-qYIvxVwBXJ
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        // Loop through each item in the data array
-        for(let item of data) {
-            // Create a new div for the product
-            let productDiv = document.createElement('div');
-            productDiv.classList.add('product', 'col-lg-4', 'col-md-6', 'mb-4');
-
-            // Create a new h2 for the product name
-            let nameH2 = document.createElement('h2');
-            nameH2.textContent = item.displayName;
-
-            // Create a new p for the date
-            let dateP = document.createElement('p');
-            dateP.textContent = `Build ${item.prodBuild} - Released ${item.updateReleaseDate}`;
-
-            // Create div for buttons
-            let buttonsDiv = document.createElement('div');
-            buttonsDiv.classList.add('buttons');
-
-            // Create a new a for the download link
-            let urlA = document.createElement('a');
-            urlA.classList.add('download');
-            urlA.textContent = 'Download';
-            urlA.href = item.url;
-
-            // Create a new a for the changelog link
-            let urlB = document.createElement('a');
-            urlB.classList.add('changes');
-            urlB.textContent = 'Changes';
-            urlB.href = item.relNotesUrl;
-            urlB.title = item.longDesc;
-            urlB.setAttribute('data-toggle', 'tooltip');
-            urlB.setAttribute('data-placement', 'bottom');
-
-            // Append the h2, p, and a to the appropriate divs
-            productDiv.appendChild(nameH2);
-            productDiv.appendChild(dateP);
-            productDiv.appendChild(buttonsDiv);
-            buttonsDiv.appendChild(urlA);
-            buttonsDiv.appendChild(urlB);
-
-            // Remove spinner
-            spinnerDiv.remove();
-
-            // Append the product div to the products div
-            productsDiv.appendChild(productDiv);
+        if (!data.length) {
+            // No updates found
+            let noProducts = document.createElement('h2');
+            noProducts.textContent = "No updates found for specified year.";
+            productsDiv.appendChild(noProducts);
+        } else {
+            // Loop through each item in the data array
+            for(let item of data) {
+                // Create a new div for the product
+                let productDiv = document.createElement('div');
+                productDiv.classList.add('product', 'col-lg-4', 'col-md-6', 'mb-4');
+                
+                // Create a new h2 for the product name
+                let nameH2 = document.createElement('h2');
+                nameH2.textContent = item.displayName;
+                
+                // Create a new p for the date
+                let dateP = document.createElement('p');
+                dateP.textContent = `Build ${item.prodBuild} - Released ${item.updateReleaseDate}`;
+                
+                // Create div for buttons
+                let buttonsDiv = document.createElement('div');
+                buttonsDiv.classList.add('buttons');
+                
+                // Create a new a for the download link
+                let urlA = document.createElement('a');
+                urlA.classList.add('download');
+                urlA.textContent = 'Download';
+                urlA.href = item.url;
+                
+                // Create a new a for the changelog link
+                let urlB = document.createElement('a');
+                urlB.classList.add('changes');
+                urlB.textContent = 'Changes';
+                urlB.href = item.relNotesUrl;
+                urlB.title = item.longDesc;
+                urlB.setAttribute('data-toggle', 'tooltip');
+                urlB.setAttribute('data-placement', 'bottom');
+                
+                // Append the h2, p, and a to the appropriate divs
+                productDiv.appendChild(nameH2);
+                productDiv.appendChild(dateP);
+                productDiv.appendChild(buttonsDiv);
+                buttonsDiv.appendChild(urlA);
+                buttonsDiv.appendChild(urlB);
+                
+                // Remove spinner
+                spinnerDiv.remove();
+                
+                // Append the product div to the products div
+                productsDiv.appendChild(productDiv);
+            }
         }
     })
     .catch(error => console.error(error));
